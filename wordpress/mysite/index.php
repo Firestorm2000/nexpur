a972fe64a2745d81ea56670a8293cfdfb526500f
Firestorm2000 <teenwolf2000@abv.bg>
2019-12-30 19:04:38 +0200

    Add files via upload

--- wordpress/mysite/index.php
+++ wordpress/mysite/index.php
@@ -1,23 +1,17 @@
-<?php get_header(); ?>
-	<div class="row">
-		<div class="col-sm-8 blog-main">
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-			<?php
-			if ( have_posts() ) : while ( have_posts() ) : the_post();
-                            get_template_part( 'content', get_post_format() );
-			endwhile;?>
-                        <nav>
-                            <ul class="pager">
-                                    <li><?php next_posts_link( 'Previous' ); ?></li>
-                                    <li><?php previous_posts_link( 'Next' ); ?></li>
-                            </ul>
-                        </nav>
-                        
-                        <?php endif;
-			?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-		</div> <!-- /.blog-main -->
-
-		<?php get_sidebar(); ?>
-	</div> <!-- /.row -->
-<?php get_footer(); ?>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
