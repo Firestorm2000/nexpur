ca89e1016de43433a22b9e9303e302803add1b5b
Firestorm2000 <teenwolf2000@abv.bg>
2020-01-09 14:11:17 -1100

    done with everything

--- wordpress/mysite/wp-content/themes/startwordpress/index.php
+++ wordpress/mysite/wp-content/themes/startwordpress/index.php
@@ -2,19 +2,20 @@
 	<div class="row">
 		<div class="col-sm-8 blog-main">
 
-				<ul> <?php query_posts('orderby=comment_count'); 
+				<ul> <?php query_posts('orderby=comment_count');
                         if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
                             <li><a href="<?php the_permalink() ?>" rel="bookmark" 
                                    title="<?php the_title_attribute(); ?>">
                                        <?php the_title(); ?> (<?php comments_number('%'); ?>)</a>
                             </li>
-							  <nav>
-                            <ul class="pager">
-                                    <li><?php next_posts_link( 'Previous' ); ?></li>
-                                    <li><?php previous_posts_link( 'Next' ); ?></li>
-                            </ul>
-                        </nav>
-							 <?php endwhile; ?> <?php else : ?>
+
+							 <?php endwhile; ?>
+                        <nav>
+                        <ul class="pager">
+                            <li><?php next_posts_link( 'Previous' ); ?></li>
+                            <li><?php previous_posts_link( 'Next' ); ?></li>
+                        </ul>
+                        </nav><?php else : ?>
                             <li>Sorry, no posts were found.
                             </li>
                         </ul>
--- wordpress/mysite/wp-content/themes/startwordpress/searchpage.php
+++ wordpress/mysite/wp-content/themes/startwordpress/searchpage.php
@@ -4,16 +4,21 @@ Template Name: search
 */
 ?>
 <?php
+global $query_string;
+
+wp_parse_str($query_string, $search_query);
+$search = new WP_Query($search_query);
+
+
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div class="wrap">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
 
-			<?php get_search_form(); ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
+                <?php get_search_form(); ?>
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div><!-- .wrap -->
 
 <?php get_footer();
